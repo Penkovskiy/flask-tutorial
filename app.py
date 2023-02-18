915d8ff4679265095d631f0e96872d280e6fe98f
Penkovskiy <36210754+Penkovskiy@users.noreply.github.com>
2023-02-18 05:35:31 +0000

    Added the Flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_wold():
+  return '<h1>Hello, world</h1>'
+
+if __name__ == '__main__':
+  app.run('0.0.0.0', debug=True)
